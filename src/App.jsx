7b06d1085be0e4dd89748a0a91687cc7e605c9d7
nomangopan <ytop44610@gmail.com>
2025-03-07 05:28:31 -0800

    fix some error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import AboutDev from "./components/Aboutdev/AboutDev"
-import AboutMe from "./components/aboutme/AboutMe"
+import AboutMe from "./components/aboutme/Aboutme"
 import Education from "./components/education/Education"
 import Header from "./components/header/Header"
 import Projects from "./components/project/Projects"
